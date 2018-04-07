db5d69c13dbd08d19965bd95542cec47f99f0efb
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-04-07 13:53:07 -0700

    removed material.js

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+(function(){
+  let canvas = document.getElementById("canvas-screen");
+
+    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('mousedown', getPosition, false);
+
+
+    function drawCanvas () {
+
+    }
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      drawCanvas();
+    }
+
+    function getPosition(e) {
+      console.log([e.clientX, e.clientY])
+      return [e.clientX, e.clientY];
+    }
+
+resizeCanvas();
+})();
